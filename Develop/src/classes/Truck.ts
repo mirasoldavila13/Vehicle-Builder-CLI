23b46fed3f658395769a4807cec6beb4f48aae93
mirasoldavila13 <mirasoldavila13@aim.com>
2024-08-19 00:12:05 -0700

    Implemented Truck class with VehicleAttributes interface, added detailed TODO comments for properties, constructor, methods, and inheritance. Created VehicleAttributes.ts interface and updated index.ts with truck instance and wheel array.

---
+++ Develop/src/classes/Truck.ts
@@ -0,0 +1,70 @@
+// import the Vehicle, Motorbike, Car, Wheel, and AbleToTow classes/interfaces
+import Vehicle from './Vehicle.js';
+import Motorbike from './Motorbike.js';
+import Car from './Car.js';
+import Wheel from './Wheel.js';
+import AbleToTow from '../interfaces/AbleToTow.js';
+import VehicleAttributes from '../interfaces/VehicleAttributes.js';
+
+class Truck extends Vehicle implements AbleToTow, VehicleAttributes{
+  vin: string;
+  color: string;
+  make: string;
+  model: string;
+  year: number;
+  weight: number;
+  topSpeed: number;
+  wheels: Wheel[];
+  towingCapacity: number;
+
+  constructor(
+    vin: string,
+    color: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number,
+    wheels: Wheel[],
+    towingCapacity: number
+  ){
+    super();
+    this.vin = vin;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
+    this.wheels = wheels.length === 4 ? wheels : [new Wheel(), new Wheel(), new Wheel(), new Wheel()];
+    this.towingCapacity = towingCapacity;
+  }
+
+
+  tow(vehicle: Truck | Motorbike | Car): void {
+    const {make, model, weight} = vehicle as VehicleAttributes;
+    if(weight <= this.towingCapacity){
+      console.log(`${this.make} ${this.model} is towing ${make} ${model}`);
+    }
+    else{
+      console.log(`${make} ${model} is too heavy to be towed by ${this.make} ${this.model}`);
+    }
+  }
+
+    override printDetails(): void {
+      super.printDetails();
+      console.log(`VIN: ${this.vin}`);
+      console.log(`Color: ${this.color}`);
+      console.log(`Make: ${this.make}`);
+      console.log(`Model: ${this.model}`);
+      console.log(`Year: ${this.year}`);
+      console.log(`Weight: ${this.weight} lbs`);
+      console.log(`Top Speed: ${this.topSpeed} mph`);
+      console.log(`Towing Capacity: ${this.towingCapacity} lbs`);
+      this.wheels.forEach((wheel, index) => {
+        console.log(`Wheel ${index + 1}: ${wheel.getDiameter} inch with a ${wheel.getTireBrand} tire`);
+      });
+    }
+}
+
+export default Truck;
---
+++ Develop/src/interfaces/VehicleAttributes.ts
@@ -0,0 +1,7 @@
+interface VehicleAttributes {
+    make: string;
+    model: string;
+    weight: number;
+}
+
+export default VehicleAttributes;
